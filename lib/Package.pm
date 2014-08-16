1adb9c20d371f21d7c6c14351cf016d7c6f7b334
Ingy döt Net <ingy@ingy.net>
2014-08-15 21:23:20 -0700

    CPAN Release 0.24
    - Remove t/use_ok.t

--- lib/Package.pm
+++ lib/Package.pm
@@ -1,5 +1,5 @@
 package Package;
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 
 #------------------------------------------------------------------------------#
 package Package::Command;
--- test/use_ok.t
+++
@@ -1,4 +0,0 @@
-use Test::More tests => 2;
-
-use_ok 'Package';
-use_ok 'Package::Conf';
